589a6d1dd0cf610e39d119f76f67f5c3a01eaaf1
Tshepo <0715tshepomatima@gmail.com>
2026-02-12 16:13:27 +0200

    Create higher or lower game.py

---
+++ higher or lower game.py
@@ -0,0 +1,49 @@
+from art import logo, vs
+from game_data import data
+import random
+
+score = 0
+
+def format_data(account):
+    account_name = account["name"]
+    account_description = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_description}, from {account_country}"
+
+def check_answer(user_guess, a_follower, b_follower):
+    if a_follower > b_follower:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+
+print(logo)
+
+
+
+game_should_continue = True
+
+while game_should_continue:
+    account_a = random.choice(data)
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    if is_correct:
+        score += 1
+        print("\n" * 20)
+        print(logo)
+        print(f"You're right! Your current score is: {score}")
+    else:
+        print(f"Sorry, that's wrong. Your final score is: {score}")
+        game_should_continue = False
+
